4c29974350dc294f8009d713b65cd933cc09b1f6
MikeIv <210415@dantser.ru>
2021-06-18 17:51:16 +0300

    add

--- src/components/ModalValidate.vue
+++ src/components/ModalValidate.vue
@@ -3,31 +3,31 @@
   <modals @close="$emit('close')" title="Modal Form width validate">
         <slot>
           <form class="modal__form" @submit.prevent="">
-            <div class="modal__form-item" :class="{ error: v$.name.$error }">
-              <label class="modal__label">Имя:</label>
-              <p class="modal__label-error" v-if="!v$.name.required">Ошибка 1</p>
-              <p class="modal__label-error" v-if="v$.name.minLength">Имя должно содержать не менее 4 символов</p>
-              <input class="modal__input"
-                     v-model="name"
-                     :class="{ errorInput: v$.name.$error }"
-                     @change="v$.name.$touch()"
-                     required>
-            </div>
-            <div class="modal__form-item">
+            <div class="modal__form-item" :class="{ error: v$.email.$error }">
               <label class="modal__label">Email:</label>
-              <input class="modal__input" v-model="contact.email">
+              <p class="modal__label-error" v-if="v$.email.required">Ошибка 2</p>
+              <p class="modal__label-error" v-if="v$.email.email">Напишите свой email</p>
+              <input class="modal__input"
+                     v-model="email"
+                     :class="{ errorInput: v$.email.$error }"
+                     @change="v$.email.$touch()"
+              >
             </div>
-            <button class="modal__btn modal__btn--submit">Отправить</button>
+            <div class="modal__form-item" :class="{ error: v$.email.$error }">
+              <label class="modal__label">Email:</label>
+              <p class="modal__label-error" v-if="v$.email.required">Ошибка 2</p>
+              <p class="modal__label-error" v-if="v$.email.email">Напишите свой email</p>
+              <input class="modal__input"
+                     v-model="email"
+                     :class="{ errorInput: v$.email.$error }"
+                     @change="v$.email.$touch()"
+                     >
+            </div>
+            <button class="modal__btn modal__btn--submit" type="submit">Отправить</button>
           </form>
         </slot>
 
 
-<!--    <div :class="{ error: v$.name.$errors.length }">-->
-<!--      <input v-model="name">-->
-<!--      <div class="input-errors" v-for="error of v$.name.$errors" :key="error.$uid">-->
-<!--        <div class="error-msg">{{ error.$message }}</div>-->
-<!--      </div>-->
-<!--    </div>-->
   </modals>
 
 </template>
@@ -38,7 +38,7 @@
 <script>
 
 import useVuelidate from '@vuelidate/core'
-import { required, minLength, email } from '@vuelidate/validators'
+import { required, email } from '@vuelidate/validators'
 
 import modals from '@/components/Modals.vue'
 
@@ -56,22 +56,19 @@ export default {
 
   data: () => ({
     name: '',
-    contact: {
-      email: ''
-    }
+    email: ''
+
   }),
 
   validations: {
       name: {
         required,
-        minLength: minLength(4)
+
       }, // Matches this.name
-      contact: {
-        email: {
+      email: {
           required,
           email
         } // Matches this.contact.email
-      }
 
   }
 
